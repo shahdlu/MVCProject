18ae3b1429e9c6dbde29b08d6485c3f9121f6540
shahdlu <117366018+shahdlu@users.noreply.github.com>
2025-04-06 13:18:32 +0200

    05 Employee Module - create Action

--- Demo.Presentation/Controllers/EmployeesController.cs
+++ Demo.Presentation/Controllers/EmployeesController.cs
@@ -1,14 +1,56 @@
-﻿using Demo.BussinessLogic.Services.Interfaces;
+﻿using Demo.BussinessLogic.DataTransferObjects.DepartmentDtos;
+using Demo.BussinessLogic.DataTransferObjects.EmployeeDtos;
+using Demo.BussinessLogic.Services.Classes;
+using Demo.BussinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presentation.Controllers
 {
-    public class EmployeesController(IEmployeeService _employeeService) : Controller
+    public class EmployeesController(IEmployeeService _employeeService, IWebHostEnvironment _environment, ILogger<EmployeesController> _logger) : Controller
     {
         public IActionResult Index()
         {
             var employees = _employeeService.GetAllEmployees();
             return View(employees);
         }
+
+        #region Create Employee
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult create(CreatedEmployeeDto employeeDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int result = _employeeService.AddEmployee(employeeDto);
+                    if (result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Employee can't be created");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (_environment.IsDevelopment())
+                    {
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    }
+                    else
+                    {
+                        _logger.LogError(ex.Message);
+                    }
+                }
+            }
+            return View(employeeDto);
+        }
+
+        #endregion
     }
 }
