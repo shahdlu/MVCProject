347b2f2d3f27ea0ed5536b176e6fb7715af3619b
shahdlu <117366018+shahdlu@users.noreply.github.com>
2025-04-06 16:02:56 +0200

    11 AntiForgeryToken

--- Demo.Presentation/Program.cs
+++ Demo.Presentation/Program.cs
@@ -4,6 +4,7 @@ using Demo.BussinessLogic.Services.Interfaces;
 using Demo.DataAccess.Data.DbContexts;
 using Demo.DataAccess.Repositories.Classes;
 using Demo.DataAccess.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Presentation
@@ -16,7 +17,10 @@ namespace Demo.Presentation
 
             #region Add services to the container
 
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
             //builderServices.AddScoped<ApplicationDbContext>();//2. Register to service in DI Container
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
